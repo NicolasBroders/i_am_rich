756e79955a59a1eb692068977faf0ad93169303b
PapailleEnQueue <contact@nicolas-broders.info>
2020-08-01 21:56:49 +0200

    add a center image fetched from the net

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text('I Am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://d1fmx1rbmqrxrr.cloudfront.net/cnet/optim/i/edit/2019/04/eso1644bsmall__w770.jpg'),
+          ),
+        ),
       ),
     ),
   );
